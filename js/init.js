a9723098e0ee35f87b61bab825c9bd54a0c2e70d
OmarXXXZavaleta <bigxshoes01@gmail.com>
2021-04-12 13:17:02 -0500

    base de datos pruebas

--- js/init.js
+++ js/init.js
@@ -1,10 +1,30 @@
 // @ts-ignore
+//firebase.initializeApp({
+  //apiKey: "AIzaSyDfJiJW2_AW1p-PKmt3w6h7kWPc9VmvzYM",
+  //authDomain: "gilpgiotx.firebaseapp.com",
+  //databaseURL: "https://gilpgiotx.firebaseio.com",
+  //projectId: "gilpgiotx",
+  //storageBucket: "gilpgiotx.appspot.com",
+  //messagingSenderId: "362186415969",
+  //appId: "1:362186415969:web:e732f8610cfdae9544ef20"
+//});
+
+  // mi firebasee
+  // Your web app's Firebase configuration
+  //var firebaseConfig = {
+  //  apiKey: "AIzaSyAnj1v66EJuXIYQRBrr-NEsDBsE-fuTgAk",
+  //  authDomain: "bxxs-iot.firebaseapp.com",
+  //  projectId: "bxxs-iot",
+  //  storageBucket: "bxxs-iot.appspot.com",
+  //   messagingSenderId: "478914869660",
+  //  appId: "1:478914869660:web:86f97f8108883028e67059"
+  //};
+  // Initialize Firebase
 firebase.initializeApp({
-  apiKey: "AIzaSyDfJiJW2_AW1p-PKmt3w6h7kWPc9VmvzYM",
-  authDomain: "gilpgiotx.firebaseapp.com",
-  databaseURL: "https://gilpgiotx.firebaseio.com",
-  projectId: "gilpgiotx",
-  storageBucket: "gilpgiotx.appspot.com",
-  messagingSenderId: "362186415969",
-  appId: "1:362186415969:web:e732f8610cfdae9544ef20"
+  apiKey: "AIzaSyAnj1v66EJuXIYQRBrr-NEsDBsE-fuTgAk",
+  authDomain: "bxxs-iot.firebaseapp.com",
+  projectId: "bxxs-iot",
+  storageBucket: "bxxs-iot.appspot.com",
+  messagingSenderId: "478914869660",
+  appId: "1:478914869660:web:86f97f8108883028e67059"
 });
